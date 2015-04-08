4ce96410059218f81fc350b48a5bf6f671a34d0e
manishn90 <manish@nathani.me>
2015-04-08 13:31:50 +0700

    edited quote

--- wp-content/themes/CedarTheme/postformat/quote.php
+++ wp-content/themes/CedarTheme/postformat/quote.php
@@ -7,6 +7,7 @@
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class("postitem"); ?>>
 		<div class="wrapper">
+			<h2 class="title"><?php the_title(); ?></h2>
 			<div class="content"><?php echo the_content(); ?></div>
 		</div>
 	</article>
